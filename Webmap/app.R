88a39ab3f53f0b56d4f5b8f9d5c9337e6ccc9e9e
Pierre Manchon <pierre.manchon@pm.me>
2020-12-15 12:43:32 +0100

    Update app.R

--- Webmap/app.R
+++ Webmap/app.R
@@ -1,11 +1,7 @@
-#
 # This is a Shiny web application. You can run the application by clicking
 # the 'Run App' button above.
-#
 # Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+#http://shiny.rstudio.com/
 
 library(shiny)
 
@@ -13,16 +9,16 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Webmapping incendies sud de france"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
             sliderInput("bins",
                         "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+                        min=1,
+                        max=50,
+                        value=30)
         ),
 
         # Show a plot of the generated distribution
@@ -38,12 +34,18 @@ server <- function(input, output) {
     output$distPlot <- renderPlot({
         # generate bins based on input$bins from ui.R
         x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+        bins <- seq(min(x),
+                    max(x),
+                    length.out=input$bins + 1)
 
         # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+        hist(x,
+             breaks=bins,
+             col='darkgray',
+             border='white')
     })
 }
 
 # Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui=ui,
+         server=server)
